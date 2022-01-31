a014ed70057ecb1d0e66b0cc0ae1bab8f6d09798
Saicharan Reddy <ashannagarisaicharanreddy@gmail.com>
2022-01-31 18:47:41 +0530

    Add project files.

---
+++ StudentRegistration/Controllers/LoginController.cs
@@ -0,0 +1,50 @@
+﻿using StudentRegistration.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentRegistration.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Details db)
+        {
+            
+
+
+                db = new Details();
+                string conn = ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
+                SqlConnection sqlConn = new SqlConnection(conn);
+            sqlConn.Open();
+            string sql = "insert into [dbo].[Student](Id,FirstName,LastName,Email,Gender,UniversityName,CollegeName,Course) values('18J41A0548','SAi','chat','xyz','m','abc','ded','dua')";
+                SqlCommand cmd = new SqlCommand(sql, sqlConn);
+           
+               // sqlConn.Open();
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("SQL Error");
+            }
+                sqlConn.Close();
+           
+            
+
+
+            return Content("Succesfully Added");
+   
+        }
+    }
+}
---
+++ StudentRegistration/Models/Details.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StudentRegistration.Models
+{
+    public class Details
+    {
+        [Required]
+        [MaxLength(8)]
+        public string Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string UniversityName { get; set; }
+        [Required]
+        public string CollegeName { get; set; }
+        [Required]       
+        public string Course { get; set; }  
+    }
+}
